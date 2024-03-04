cd340e2770c0ae2c71d6a900d07c3f99b38d374f
Rockn26 <irfanank26@gmail.com>
2024-03-04 16:08:43 +0300

    add flex

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.style.css'
 
 const Navbar = () => {
   return (
-    <div>
-      burası navbar
+    <div className='navbar-component'>
+        <Link to='/'>Home</Link>
+        <Link to='/animal'>Animal</Link>
+        <Link to='/appointment'>Appointment</Link>
+        <Link to='/customer'>Customer</Link>
+        <Link to='/doctor'>Doctor</Link>
+        <Link to='/report'>Report</Link>
+        <Link to='/vaccine'>Vaccine</Link>
     </div>
   )
 }
