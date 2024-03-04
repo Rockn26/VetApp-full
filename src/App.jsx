aa8b7c70aaaf803dc8f8e01265628c643c596ca4
Rockn26 <irfanank26@gmail.com>
2024-03-04 16:03:53 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 
 import React from 'react'
+import { Routes, Route  } from 'react-router-dom'
+import AnimalPage from './pages/AnimalPage/AnimalPage'
+import AppointmentPage from './pages/AppointmentPage/AppointmentPage'
+import CustomerPage from './pages/CustomerPage/CustomerPage'
+import DoctorPage from './pages/DoctorPage/DoctorPage'
+import ReportPage from './pages/ReportPage/ReportPage'
+import VaccinePage from './pages/VaccinePage/VaccinePage'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/animal" element={<AnimalPage />} />
+        <Route path="/appointment" element={<AppointmentPage />} />
+        <Route path="/customer" element={<CustomerPage />} />
+        <Route path='/doctor' element={<DoctorPage />} />
+        <Route path='/report' element={<ReportPage />} />
+        <Route path='/vaccine' element={<VaccinePage />} />
+      </Routes>
     </div>
   )
 }
