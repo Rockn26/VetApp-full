dec9be9efd500063d2df35eb3a4bb77012a3f9b4
Rockn26 <irfanank26@gmail.com>
2024-03-04 18:08:35 +0300

    implement user input

--- src/pages/CustomerPage/CustomerPage.jsx
+++ src/pages/CustomerPage/CustomerPage.jsx
@@ -1,17 +1,43 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { getCustomers } from "../../api/Customer";
+import { createCustomer, getCustomers } from "../../api/Customer";
 import "./CustomerPage.style.css";
+import Customer from "../../components/Customer/Customer";
 
 const CustomerPage = () => {
   const [customers, setCustomers] = useState([]);
-
   useEffect(() => {
     getCustomers().then((data) => {
       setCustomers(data);
     });
-
   }, []);
+
+  const [customer, setCustomer] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    city: "",
+    address: "",
+  });
+
+  const handleChange = (e) => {
+    setCustomer({ ...customer, [e.target.name]: e.target.value });
+  };
+
+  const handleAdd = () => {
+    createCustomer(customer).then((data) => {
+      setCustomers(prev => [...prev, data]);
+      setCustomer({
+        name: "",
+        email: "",
+        phone: "",
+        city: "",
+        address: "",
+      });
+    }
+    );
+  };
+
   return (
     <div className="customer-page-component">
       <h2>Customer Management</h2>
@@ -22,21 +48,52 @@ const CustomerPage = () => {
           that are registered in the system.
         </p>
         {customers.map((customer) => (
-          <div key={customer.id} className="customer-item">
-            <p>{customer.name}</p>
-            <p>{customer.email}</p>
-            <p>{customer.phone}</p>
-            <p>{customer.city}</p>
-            <p>{customer.address}</p>
-          </div>
+          <Customer
+            key={customer.id}
+            customerProp={customer}
+            setCustomers={setCustomers}
+          />
         ))}
       </div>
       <div className="customer-add">
-        <input type="text" name="name" placeholder="name" />
-        <input type="text" name="email" placeholder="email" />
-        <input type="text" name="phone" placeholder="phone" />
-        <input type="text" name="city" placeholder="city" />
-        <input type="text" name="address" placeholder="address" />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="name"
+          placeholder="name"
+          value={customer.name}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="email"
+          placeholder="email"
+          value={customer.email}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="phone"
+          placeholder="phone"
+          value={customer.phone}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="city"
+          placeholder="city"
+          value={customer.city}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="address"
+          placeholder="address"
+          value={customer.address}
+        />
+        <button onClick={handleAdd} className="customer-add-button">
+          Add
+        </button>
       </div>
     </div>
   );
