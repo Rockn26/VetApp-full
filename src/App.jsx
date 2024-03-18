3a3f44f0a1809645cd184ba0afc4d6dbe2c1b3dd
Rockn26 <irfanank26@gmail.com>
2024-03-18 14:48:03 +0300

    add show alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes, Route  } from 'react-router-dom'
 import AnimalPage from './pages/AnimalPage/AnimalPage'
 import AppointmentPage from './pages/AppointmentPage/AppointmentPage'
@@ -8,12 +8,18 @@ import DoctorPage from './pages/DoctorPage/DoctorPage'
 import ReportPage from './pages/ReportPage/ReportPage'
 import VaccinePage from './pages/VaccinePage/VaccinePage'
 import Navbar from './components/Navbar/Navbar'
+import HomePage from './pages/HomePage/HomePage'
+import ErrorContext from './context/error/ErrorContext'
+import "./App.css";
 
 const App = () => {
+  const {showAlert, alertMessage}= useContext(ErrorContext);
   return (
     <div>
       <Navbar />
+      {showAlert && <div className='alert-message'>{alertMessage}</div>}
       <Routes>
+        <Route path='/' element={<HomePage />} />
         <Route path="/animal" element={<AnimalPage />} />
         <Route path="/appointment" element={<AppointmentPage />} />
         <Route path="/customer" element={<CustomerPage />} />
