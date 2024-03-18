89640b3c29ca25e53109dfa1187dde14ef9e12ca
Rockn26 <irfanank26@gmail.com>
2024-03-18 14:47:45 +0300

    add contextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ErrorContextProvider from "./context/error/ErrorContextProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
-    <App />
-   </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <ErrorContextProvider>
+            <App />
+        </ErrorContextProvider>
+    </BrowserRouter>
+);
